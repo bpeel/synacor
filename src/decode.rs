064fd9c95b847fd979ded83a3a3a3da08b81309d
Neil Roberts <bpeeluk@yahoo.co.uk>
2017-05-15 16:51:27 +0100

    Add a program to decode the encrypted parts

---
+++ src/decode.rs
@@ -0,0 +1,60 @@
+use std::io::Write;
+use std::io::Read;
+use std::error::Error;
+
+macro_rules! println_stderr(
+    ($($arg:tt)*) => { {
+        let r = writeln!(&mut ::std::io::stderr(), $($arg)*);
+        r.expect("failed printing to stderr");
+    } }
+);
+
+
+fn fetch(f: &mut std::io::Stdin) -> Result<Option<u16>, std::io::Error> {
+    let mut buf = [0 as u8; 2];
+
+    let n = f.read(&mut buf)?;
+
+    if n < 2 {
+        Ok(None)
+    } else {
+        Ok(Some((buf[0] as u16) | ((buf[1] as u16) << 8)))
+    }
+}
+
+fn decrypt(input: &mut std::io::Stdin,
+           output: &mut std::io::Stdout) -> Result<(), std::io::Error> {
+    let mut addr: u16 = 0;
+
+    loop {
+        match fetch(input)? {
+            Some(val) => {
+                let square_address = ((addr as u32 * addr as u32) &
+                                      0x7fff) as u16;
+                let decrypted = val ^ square_address ^ 0x4154;
+                let buf: [u8; 2] = [
+                    decrypted as u8,
+                    (decrypted >> 8) as u8
+                ];
+                output.write(&buf)?;
+                addr += 1;
+            },
+            None => break
+        }
+    }
+
+    Ok(())
+}
+
+fn main() {
+    let mut stdin = std::io::stdin();
+    let mut stdout = std::io::stdout();
+
+    match decrypt(&mut stdin, &mut stdout) {
+        Err(e) => {
+            println_stderr!("{}", e.description());
+            std::process::exit(1);
+        },
+        Ok(_) => ()
+    }
+}

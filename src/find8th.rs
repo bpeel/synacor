151251967cfd8789afcd8b0beffb8d320a3dd257
Neil Roberts <bpeeluk@yahoo.co.uk>
2017-05-17 15:51:45 +0200

    find8th: Make a struct to handle caluclating the value

--- src/find8th.rs
+++ src/find8th.rs
@@ -2,28 +2,41 @@ fn minus_one(a: u16) -> u16 {
     ((a as u32 + 0x7fff) & 0x7fff) as u16
 }
 
-fn thing1(eighth: u16, a: u16, b: u16) -> u16 {
-    if a != 0 {
-        thing2(eighth, a, b)
-    } else {
-        b + 1
-    }
+struct Finder {
+    eighth: u16
 }
 
-fn thing2(eighth: u16, a: u16, b: u16) -> u16 {
-    if b != 0 {
-        thing3(eighth, a, b)
-    } else {
-        thing1(eighth, minus_one(a), eighth)
+impl Finder {
+    fn new(eighth: u16) -> Finder {
+        Finder {
+            eighth: eighth
+        }
     }
-}
 
-fn thing3(eighth: u16, a: u16, b: u16) -> u16 {
-    thing1(eighth, minus_one(a), thing1(eighth, a, minus_one(b)))
+    fn thing1(&self, a: u16, b: u16) -> u16 {
+        if a != 0 {
+            self.thing2(a, b)
+        } else {
+            b + 1
+        }
+    }
+
+    fn thing2(&self, a: u16, b: u16) -> u16 {
+        if b != 0 {
+            self.thing3(a, b)
+        } else {
+            self.thing1(minus_one(a), self.eighth)
+        }
+    }
+
+    fn thing3(&self, a: u16, b: u16) -> u16 {
+        self.thing1(minus_one(a), self.thing1(a, minus_one(b)))
+    }
 }
 
 fn main() {
     for eighth in 0..0x8000 {
-        println!("{:04x} {:04x}", eighth, thing1(eighth, 4, 1))
+        let finder = Finder::new(eighth);
+        println!("{:04x} {:04x}", eighth, finder.thing1(4, 1))
     }
 }

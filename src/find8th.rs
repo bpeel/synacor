57b1eb68af56d7df78f3def0973df2c8612a3f70
Neil Roberts <bpeeluk@yahoo.co.uk>
2017-05-18 17:18:20 +0200

    find8th: Combine the three functions into one
    This makes it easier to reason about the code

--- src/find8th.rs
+++ src/find8th.rs
@@ -20,35 +20,31 @@ impl Finder {
         }
     }
 
-    fn thing1(&mut self, a: u16, b: u16) -> u16 {
+    fn compute_uncached(&mut self, a: u16, b: u16) -> u16 {
+        if a != 0 {
+            if b != 0 {
+                let b = self.compute(a, minus_one(b));
+                self.compute(minus_one(a), b)
+            } else {
+                let b = self.eighth;
+                self.compute(minus_one(a), b)
+            }
+        } else {
+            b + 1
+        }
+    }
+
+    fn compute(&mut self, a: u16, b: u16) -> u16 {
         let key = (a, b);
 
         match self.cache.get(&key).cloned() {
             Some(n) => n,
             None => {
-                let v = if a != 0 {
-                    self.thing2(a, b)
-                } else {
-                    b + 1
-                };
+                let v = self.compute_uncached(a, b);
                 self.cache.insert(key, v);
                 v
             }
         }
-    }
-
-    fn thing2(&mut self, a: u16, b: u16) -> u16 {
-        if b != 0 {
-            self.thing3(a, b)
-        } else {
-            let b = self.eighth;
-            self.thing1(minus_one(a), b)
-        }
-    }
-
-    fn thing3(&mut self, a: u16, b: u16) -> u16 {
-        let b = self.thing1(a, minus_one(b));
-        self.thing1(minus_one(a), b)
     }
 }
 
@@ -58,7 +54,7 @@ fn find_solution(offset: u16) {
     while eighth <= 0x7fff {
         let mut finder = Finder::new(eighth);
 
-        let n = finder.thing1(4, 1);
+        let n = finder.compute(4, 1);
         if n == 6 {
             println!("{:04x} {:04x}", eighth, n);
         }
